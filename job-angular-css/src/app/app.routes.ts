acc3da86eae2ea1296091d645b686471fd9d1176
Naveen Manjappa <NManjappa@manh.com>
2024-11-12 17:02:47 +0000

    Add routing and starter components

--- job-angular-css/src/app/app.routes.ts
+++ job-angular-css/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { AddJobComponent } from './add-job/add-job.component';
 
 export const routes: Routes = [
   {
-    path:'',
+    path:'home',
     component:HomeComponent
+  },
+  {
+    path:'jobs',
+    component:JobsComponent
+  },
+  {
+    path:'add-job',
+    component:AddJobComponent
   }
 ];
