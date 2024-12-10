dd8e478aba8832d118ffeaf6bf0172ccd05ff40b
Naveen Manjappa <NManjappa@manh.com>
2024-12-10 12:55:40 +0000

    job-angular-css Create Add Job page

--- job-angular-css/src/app/add-job/add-job.component.ts
+++ job-angular-css/src/app/add-job/add-job.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-job',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add-job.component.html',
   styleUrl: './add-job.component.css'
 })
 export class AddJobComponent {
+  jobTypes = ['Full-Time','Part-Time','Remote','Internship'];
 
+  salaryRanges = [
+    "Under $50K",
+    "$50K - $60K",
+    "$60K - $70K",
+    "$70K - $80K",
+    "$80K - $90K",
+    "$90K - $100K",
+    "$100K - $125K",
+    "$125K - $150K",
+    "$150K - $175K",
+    "$175K - $200K",
+    "Over $200K"
+  ];
 }
