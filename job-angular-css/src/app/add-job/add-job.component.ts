fb88b22e0d18d3a2901de340187cd86109565b77
Naveen Manjappa <NManjappa@manh.com>
2024-12-10 15:33:42 +0000

    job-angular-css Add validations to Add job page

--- job-angular-css/src/app/add-job/add-job.component.ts
+++ job-angular-css/src/app/add-job/add-job.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-job',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './add-job.component.html',
   styleUrl: './add-job.component.css'
 })
-export class AddJobComponent {
+export class AddJobComponent implements OnInit {
+
   jobTypes = ['Full-Time','Part-Time','Remote','Internship'];
 
   salaryRanges = [
@@ -24,4 +26,45 @@ export class AddJobComponent {
     "$175K - $200K",
     "Over $200K"
   ];
+
+  jobForm!: FormGroup;
+  submitted =false;
+
+  ngOnInit(): void {
+    this.jobForm = new FormGroup({
+      type: new FormControl(this.jobTypes[0]),
+      jobName:new FormControl('',{validators: [Validators.required,Validators.minLength(3)]}),
+      description:new FormControl(),
+      salary:new FormControl(this.salaryRanges[0]),
+      location:new FormControl('',Validators.required),
+      companyInfo:new FormGroup({
+        company:new FormControl(),
+        description:new FormControl(),
+        email:new FormControl('',{validators:[ Validators.required,Validators.email] }),
+        phone:new FormControl()
+      })
+
+
+    });
+  }
+
+  get jobName() {
+    return this.jobForm.get('jobName');
+  }
+
+  get location() {
+    return this.jobForm.get('location');
+  }
+
+  get email() {
+    return this.jobForm.get('companyInfo.email');
+  }
+
+  CreateJob() {
+    this.submitted = true;
+    console.log(this.jobForm.controls);
+    if(this.jobForm.invalid) return;
+  }
+
+
 }
