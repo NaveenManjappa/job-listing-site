3cc23ff43b1ad3f83af8a3b96a249d1adf36f726
Naveen Manjappa <NManjappa@manh.com>
2024-12-17 17:27:03 +0000

    job-angular-css Adding HomeCard component

--- job-angular-css/src/app/home/home.component.ts
+++ job-angular-css/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
+import { HomeCardComponent } from "./home-card/home-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, HomeCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
