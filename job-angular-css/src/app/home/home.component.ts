bed5a8e7ca0c41c02241b4dee29c6f9494c51b66
Naveen Manjappa <NManjappa@manh.com>
2024-12-18 11:21:04 +0000

    job-angular-css Add job listings page

--- job-angular-css/src/app/home/home.component.ts
+++ job-angular-css/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { HomeCardComponent } from "./home-card/home-card.component";
+import { JobListingsComponent } from "./job-listings/job-listings.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, HomeCardComponent],
+  imports: [HeroComponent, HomeCardComponent, JobListingsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ job-angular-css/src/app/home/view-all-jobs/view-all-jobs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-jobs',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-all-jobs.component.html',
+  styleUrl: './view-all-jobs.component.css'
+})
+export class ViewAllJobsComponent {
+
+}
